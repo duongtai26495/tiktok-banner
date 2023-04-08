45fec3eb86959b2636573b12f252cf38394c5a4b
unknown <taiduong.264@gmail.com>
2023-04-08 15:39:39 +0700

    Source base

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='welcome'>Ai cho mày vào TikTok ?</h1>
+      <a className='get-out' href='https://www.google.com'>Đi ra</a>
+    </div>
+  );
+}
+
+export default App;
